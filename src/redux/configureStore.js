4edde13a4372d38274bb59ab9f515544d08c6504
FabriceTchekou <63434998+FabriceTchekou@users.noreply.github.com>
2020-06-19 18:50:50 -0700

    Redux Thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,7 @@
-import {
-    createStore,
-    combineReducers
-} from "redux";
+import {createStore, combineReducers, applyMiddleware } from 'redux';
+import { createForms } from 'react-redux-form';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import {
     Campsites
 } from "./campsites";
@@ -14,6 +14,7 @@ import {
 import {
     Promotions
 } from "./promotions";
+import { InitialFeedback } from './forms';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -21,8 +22,12 @@ export const ConfigureStore = () => {
             campsites: Campsites,
             comments: Comments,
             partners: Partners,
-            promotions: Promotions
-        })
+            promotions: Promotions,
+            ...createForms({
+                feedbackForm: InitialFeedback
+            })
+        }),
+        applyMiddleware(thunk, logger)
     );
 
     return store
